7456714a27ccfcafd1c10bcbdab6a3efac980961
Gareth Hogan <gareth.hogan@protectline.co.uk>
2026-06-03 00:21:49 +0100

    added polyfills for php 7.x support

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -2,6 +2,32 @@
 
 declare(strict_types=1);
 
+if (!function_exists('str_contains')) {
+    function str_contains(string $haystack, string $needle): bool
+    {
+        return $needle === '' || strpos($haystack, $needle) !== false;
+    }
+}
+
+if (!function_exists('str_starts_with')) {
+    function str_starts_with(string $haystack, string $needle): bool
+    {
+        return $needle === '' || strncmp($haystack, $needle, strlen($needle)) === 0;
+    }
+}
+
+if (!function_exists('str_ends_with')) {
+    function str_ends_with(string $haystack, string $needle): bool
+    {
+        if ($needle === '') {
+            return true;
+        }
+        $needleLength = strlen($needle);
+
+        return $needleLength <= strlen($haystack) && substr($haystack, -$needleLength) === $needle;
+    }
+}
+
 function load_config(): array
 {
     $configPath = dirname(__DIR__) . '/config.php';
